0b68c5d0eca6fe6e9adfcb7797fed6faed3c3aa5
Tsoj Tsoj <tsoj.tsoj@gmx.de>
2024-09-01 18:10:33 +0200

    Added sized type specifier

--- grammar.js
+++ grammar.js
@@ -115,6 +115,8 @@ module.exports = grammar(CPP1, {
     ],
     [$.call_expression, $.cpp2_primitive_type],
     [$.user_defined_literal, $.cpp2_user_defined_literal],
+    [$.sized_type_specifier, $.cpp2_primitive_type],
+    [$.sized_type_specifier, $.cpp2_ordinary_identifier],
 
     // Cpp2
     [$.cpp2_no_namespace_identifier, $.cpp2_template_identifier],
@@ -675,6 +677,7 @@ module.exports = grammar(CPP1, {
     cpp2_primitive_type: ($) =>
       choice(
         $.primitive_type,
+        $.sized_type_specifier,
         token(
           choice(
             "ushort",
